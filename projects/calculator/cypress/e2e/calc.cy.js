a55417c8383c7a22fd457ddd52e164232e936ae4
Jossrec <a00837812@tec.mx>
2025-03-10 12:03:27 -0600

    commit calculadora

--- projects/calculator/cypress/e2e/calc.cy.js
+++ projects/calculator/cypress/e2e/calc.cy.js
@@ -1,10 +1,21 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('prueba funcionalidad calculadora', () => {
+  it('prueba multiplicación', () => {
     cy.visit('http://localhost:3000/')
     cy.contains('AC').click()
     cy.contains('2').click()
     cy.contains('x').click()
     cy.contains('6').click()
-    cy.get('.component-display').first().should('have.text', '6')
+    cy.contains('=').click()
+    cy.get('.component-display').first().should('have.text', '12')
+  })
+
+  it('prueba resta', () => {
+    cy.visit('http://localhost:3000/')
+    cy.contains('AC').click()
+    cy.contains('7').click()
+    cy.get(':nth-child(3) > .orange > button').click()
+    cy.contains('6').click()
+    cy.contains('=').click()
+    cy.get('.component-display').first().should('have.text', '1')
   })
 })
